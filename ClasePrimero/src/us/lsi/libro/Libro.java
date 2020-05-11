fcb40b4a0255b4aa47e7d0589061a865dd839fd2
migueltoro <migueltoro@LAPTOP-93NQHUSI.lsi.us.es>
2020-05-11 12:32:53 +0200

    11/05/2020

--- ClasePrimero/src/us/lsi/libro/Libro.java
+++ ClasePrimero/src/us/lsi/libro/Libro.java
@@ -4,9 +4,14 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.Arrays;
+import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
+import us.lsi.tools.Enumerate;
 import us.lsi.tools.FileTools;
+import us.lsi.tools.StreamTools;
 
 public class Libro {
 	
@@ -47,5 +52,79 @@ public class Libro {
 				.getAsDouble();
 	}
 	
+	public static String primeraLineaConPalabra(String fichero, String palabra) {
+		return FileTools.streamFromFile(fichero)
+				.filter(linea->linea.contains(palabra))
+				.findFirst()
+				.get();			
+	}
+	
+	
+	public static Integer primeraNumeroLineaConPalabra(String fichero, String palabra) {
+		Stream<String> lineas = FileTools.streamFromFile(fichero);
+		Stream<Enumerate<String>> lineasNumeros = StreamTools.enumerate(lineas);
+		return lineasNumeros.filter(e->e.value.contains(palabra))
+				.findFirst()
+				.get()
+				.counter;
+	}
+	
+	public static String lineaNumero(String fichero, Integer n) {
+		Stream<String> lineas = FileTools.streamFromFile(fichero);
+		Stream<Enumerate<String>> lineasNumeros = StreamTools.enumerate(lineas);
+		return lineasNumeros.filter(e->e.counter.equals(n))
+				.findFirst()
+				.get()
+				.value;
+	}
+	
+	public static Map<String,Long> frecuenciasDePalabras1(String fichero) {
+		return FileTools.streamFromFile(fichero)
+				.flatMap(linea->Arrays.stream(linea.split("[ ,;.\n()?¿!¡:\"]")))
+				.collect(Collectors.groupingBy(
+						x->x,
+						Collectors.counting()));				
+	}
+	
+	public static Map<String,Integer> frecuenciasDePalabras2(String fichero) {
+		return FileTools.streamFromFile(fichero)
+				.flatMap(linea->Arrays.stream(linea.split("[ ,;.\n()?¿!¡:\"]")))
+				.filter(p->p.length() >0)
+				.collect(Collectors.groupingBy(
+						x->x,
+						Collectors.collectingAndThen(Collectors.counting(),x->x.intValue())));				
+	}
+	
+	public static SortedMap<String,Integer> frecuenciasDePalabras3(String fichero) {
+		return FileTools.streamFromFile(fichero)
+				.flatMap(linea->Arrays.stream(linea.split("[ ,;.\n()?¿!¡:\"]")))
+				.filter(p->p.length() >0)
+				.collect(Collectors.groupingBy(
+						x->x,
+						()->new TreeMap<>(),
+						Collectors.collectingAndThen(Collectors.counting(),x->x.intValue())));				
+	}
+	
+	public static SortedMap<Integer,Set<String>> palabrasPorFrecuencias(String fichero) {
+		SortedMap<String,Integer> m = frecuenciasDePalabras3(fichero);
+		return m.keySet().stream()
+				.collect(Collectors.groupingBy(
+						x->m.get(x),
+						()->new TreeMap<>(),
+						Collectors.toSet()));	
+	}
+	
+	public static SortedMap<String,Set<Integer>> lineasDePalabra(String fichero) {
+		Stream<String> lineas = FileTools.streamFromFile(fichero);
+		Stream<Enumerate<String>> lineasNumeros = StreamTools.enumerate(lineas);
+		return lineasNumeros.
+				.flatMap(numeroLinea->Arrays.stream(numeroLinea.value.split("[ ,;.\n()?¿!¡:\"]"))
+						.map(p->Enumerate.of(numeroLinea.counter,p)))
+				.filter(p->p.length() >0)
+				.collect(Collectors.groupingBy(
+						x->x,
+						()->new TreeMap<>(),
+						Collectors.collectingAndThen(Collectors.counting(),x->x.intValue()))
+	}
 
 }
--- ClasePrimero/src/us/lsi/libro/Test.java
+++ ClasePrimero/src/us/lsi/libro/Test.java
@@ -1,16 +1,27 @@
 package us.lsi.libro;
 
 import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.SortedMap;
+import java.util.stream.Collectors;
 
 public class Test {
 
 	public static void main(String[] args) {
 		Locale.setDefault(new Locale("en", "US"));
-		System.out.println(Libro.numeroDeLineas("ficheros/quijote.txt"));
-		System.out.println(Libro.numeroDePalabrasDistintas("ficheros/quijote.txt"));
-		System.out.println(Libro.numeroDePalabrasDistintasNoHuecas("ficheros/quijote.txt"));
-		System.out.println(Libro.numeroDeLineasVacias("ficheros/quijote.txt"));
-		System.out.println(String.format("%.2f",Libro.longitudMediaDeLineas("ficheros/quijote.txt")));
+//		System.out.println(Libro.numeroDeLineas("ficheros/quijote.txt"));
+//		System.out.println(Libro.numeroDePalabrasDistintas("ficheros/quijote.txt"));
+//		System.out.println(Libro.numeroDePalabrasDistintasNoHuecas("ficheros/quijote.txt"));
+//		System.out.println(Libro.numeroDeLineasVacias("ficheros/quijote.txt"));
+//		System.out.println(String.format("%.2f",Libro.longitudMediaDeLineas("ficheros/quijote.txt")));
+//		System.out.println(String.format("%s",Libro.primeraLineaConPalabra("ficheros/quijote.txt","padre")));	
+//		System.out.println(String.format("%s",Libro.primeraNumeroLineaConPalabra("ficheros/quijote.txt","padre")));	
+//		System.out.println(String.format("%s",Libro.lineaNumero("ficheros/quijote.txt",300)));	
+		SortedMap<Integer, Set<String>> r = Libro.palabrasPorFrecuencias("ficheros/quijote.txt");
+//		System.out.println(String.format("(%s,%s)",1,r.get(1)));
+		System.out.println(r.keySet().stream().map(k->String.format("(%s,%s)",k,r.get(k))).collect(Collectors.joining("\n")));
+
 	}
 
 }
