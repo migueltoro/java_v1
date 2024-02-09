00c19a7800050d2a08b857629381053c0b882c72
migueltoro <migueltoro@pc01>
2024-02-09 20:22:10 +0100

    09/02/2024

--- ClasePrimero/src/us/lsi/ejemplos_b1_tipos/Direccion.java
+++ ClasePrimero/src/us/lsi/ejemplos_b1_tipos/Direccion.java
@@ -1,14 +1,35 @@
 package us.lsi.ejemplos_b1_tipos;
 
-public record Direccion(String calle, String ciudad, Integer zipCode) {
-	    
-	    public static Direccion parse(String text) {
-	        String[] partes = text.split(";");
-	        return new Direccion(partes[0],partes[1],Integer.parseInt(partes[2]));
-	    }
-	    
-	    public String toString() {
-	        return String.format("%s,%s",this.calle(),this.ciudad(),this.zipCode());
-	    }
+public record Direccion(String calle, String ciudad, Integer zipCode) implements Comparable<Direccion> {
+
+	public static Direccion of(String calle, String ciudad, Integer zipCode) {
+		return new Direccion(calle, ciudad, zipCode);
+	}
+
+	public static Direccion parse(String text) {
+		String[] partes = text.split(";");
+		return Direccion.of(partes[0], partes[1], Integer.parseInt(partes[2]));
+	}
+
+	public String toString() {
+		return String.format("%s,%s,%d", this.calle(), this.ciudad(), this.zipCode());
+	}
+
+	@Override
+	public int compareTo(Direccion other) {
+		Integer r = this.zipCode().compareTo(other.zipCode());
+		if (r == 0) r = this.ciudad().compareTo(other.ciudad());
+		if (r == 0) r = this.calle().compareTo(other.calle());
+		return r;
+
+	}
+
+	public static void main(String[] args) {
+		Direccion d1 = Direccion.parse("Callejón Virginia Collado 21;Lugo;37687");
+		System.out.println(d1);
+		Direccion d2 = Direccion.of("Victoria", "Córdoba", 42000);
+		System.out.println(d2);
+		System.out.println(d1.compareTo(d2));
+	}
 
 }
--- ClasePrimero/src/us/lsi/ejemplos_b1_tipos/Persona.java
+++ ClasePrimero/src/us/lsi/ejemplos_b1_tipos/Persona.java
@@ -6,18 +6,23 @@ import java.time.LocalDateTime;
 import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.Objects;
+
+import us.lsi.tools.Preconditions;
 
 public class Persona implements Comparable<Persona> {
 	
 	public static Persona of(String apellidos, String nombre, LocalDateTime fechaNacimiento, String dni,
 			String telefono, Direccion direccion) {
-		return new Persona(apellidos, nombre,fechaNacimiento, dni, telefono, direccion);
+		Preconditions.checkArgument(Persona.dniCorrecto(dni),String.format("El dni %s no es correcto",dni));
+		return new Persona(apellidos, nombre, fechaNacimiento, dni, telefono, direccion);
 	}
-	
+
+//Plaza Rivero,Apolinar,72842943B,1999-01-17 19:14,+34664759382,Callejón Virginia Collado 21;Lugo;37687
 	public static Persona parse(String text) {
 		String[] partes = text.split(",");
 		String dni = partes[2].strip();       
-		LocalDateTime ld = LocalDateTime.parse(partes[3], DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm"));
+		LocalDateTime ld = LocalDateTime.parse(partes[3], DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
 		String telefono = partes[4].strip();
 		Direccion direccion = Direccion.parse(partes[5].strip());
 		return Persona.of(partes[0],partes[1],ld,dni,telefono,direccion);
@@ -48,7 +53,7 @@ public class Persona implements Comparable<Persona> {
 		this.telefono = telefono;
 		this.direccion = direccion;
 	}
-
+	
 	public String apellidos() {
 		return apellidos;
 	}
@@ -75,11 +80,11 @@ public class Persona implements Comparable<Persona> {
 
 	public Integer edad() {
 		LocalDateTime now = LocalDateTime.now();
-		Period p = Period.between(fechaNacimiento.toLocalDate(), now.toLocalDate());
+		Period p = Period.between(this.fechaNacimiento.toLocalDate(), now.toLocalDate());
 		return p.getYears();
 	}
 
-	public String nombre_completo() {
+	public String nombreCompleto() {
 		return String.format("%s %s", this.nombre(), this.apellidos());
 	}
 
@@ -96,27 +101,50 @@ public class Persona implements Comparable<Persona> {
 	public String toString() {
 		String fs = this.fechaNacimiento().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
 		String hs = this.fechaNacimiento().toLocalTime().format(DateTimeFormatter.ofPattern("HH:mm"));
-		return String.format("%s, de %d, nacido el %s a las %s", nombre_completo(), edad(), fs, hs);
+		return String.format("%s, de %d, nacido el %s a las %s", this.nombreCompleto(), this.edad(), fs, hs);
 	}
 
 	@Override
 	public int compareTo(Persona other) {
 		int r = this.apellidos().compareTo(other.apellidos());
-		if (r == 0)
-			r = this.nombre().compareTo(other.nombre());
+		if (r == 0) r = this.nombre().compareTo(other.nombre());
 		return r;
+	}
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(apellidos, direccion, dni, fechaNacimiento, nombre, telefono);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Persona other = (Persona) obj;
+		return Objects.equals(apellidos, other.apellidos) && Objects.equals(direccion, other.direccion)
+				&& Objects.equals(dni, other.dni) && Objects.equals(fechaNacimiento, other.fechaNacimiento)
+				&& Objects.equals(nombre, other.nombre) && Objects.equals(telefono, other.telefono);
+	}
+	
+	private static Boolean dniCorrecto(String dni) {
+		return null;
 	}
 
 	public static void main(String[] args) {
 //		Locale.setDefault(new Locale("es", "Es"));
 //		Persona p = Persona.of("Antonio","Ramirez",LocalDateTime.of(1980,Month.APRIL,14,14,20));
-//		Persona p2 = Persona.parse("Juan Balmez,10-03-1991,02:31");
-//		System.out.println(p);
+		Persona p2 = Persona.parse("Plaza Rivero,Apolinar,72842943B,1999-01-17 19:14,+34664759382,Callejón Virginia Collado 21;Lugo;37687");
+		System.out.println(p2.dni());
 //		System.out.println(p.edad());
 //		System.out.println(p2);
 //		System.out.println(p2.edad());
 //		System.out.println(p2.diaSemanaSiguienteCumple());
 //		System.out.println(p2.siguienteCumple().getDayOfMonth());
 	}
+	
 
 }
