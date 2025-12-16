46b52ee694126f9e76966557b05ec4a0adde84ab
migueltoro <migueltoro@10.2.0.2>
2025-12-16 12:28:22 +0100

    16/12/2025

---
+++ ClasePrimero/src/us/lsi/centro/Questions.java
@@ -0,0 +1,23 @@
+package us.lsi.centro;
+
+public class Questions {
+	
+
+//		¿Cuál es el profesor con mayor número de alumnos?
+//		¿Qué profesor imparte más asignaturas en total?
+//		¿Cuántos alumnos hay por cada grupo?
+//		¿Cuál es la asignatura con mayor número de alumnos matriculados?
+//		¿Qué profesor imparte mayor número de asignaturas?
+//		¿Qué alumno tiene la mejor nota de entrada?
+//		¿Cuántos profesores hay?
+//		¿Existen asignaturas que no tienen ningún alumno matriculado?
+//		¿Cuál es el promedio de alumnos por asignatura?
+//		¿Qué profesor tiene el mayor porcentaje de aalumnos con nota de entrada superior a n? Con 10>=n<=5.
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
